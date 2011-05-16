8918e6cf212a9b59cbaec92668970e4689b9c08f
unger <unger@358b74ad-2626-0410-af80-f4c3c349ae55>
2011-05-16 12:18:44 +0000

    added shared memory rof model
    git-svn-id: https://svn.icg.tugraz.at/svn/vmgpu/trunk/vmlibraries/imageutilities@11105 358b74ad-2626-0410-af80-f4c3c349ae55

--- tests/iusparse_unittests/iu_sparse_compare.cpp
+++ tests/iusparse_unittests/iu_sparse_compare.cpp
@@ -13,7 +13,7 @@
 
 
 #define NSIZES 2
-#define NALG   2
+#define NALG   4
 
 //cusparseHandle_t handle = 0;
 
@@ -24,14 +24,23 @@ void rof_primal_dual(iu::ImageGpu_32f_C1* device_f, iu::ImageGpu_32f_C1* device_
                      float lambda, int max_iter);
 
 void rof_primal_dual_sparse(iu::SparseMatrixGpu_32f* G,
-                     iu::ImageGpu_32f_C1* device_f, iu::ImageGpu_32f_C1* device_u,
-                     iu::ImageGpu_32f_C1* device_u_, iu::ImageGpu_32f_C2* device_p,
-                     float lambda, int max_iter);
+                            iu::ImageGpu_32f_C1* device_f, iu::ImageGpu_32f_C1* device_u,
+                            iu::ImageGpu_32f_C1* device_u_, iu::ImageGpu_32f_C2* device_p,
+                            float lambda, int max_iter);
+
+void rof_primal_dual_notex(iu::ImageGpu_32f_C1* device_f, iu::ImageGpu_32f_C1* device_u,
+                           iu::ImageGpu_32f_C1* device_u_, iu::ImageGpu_32f_C2* device_p,
+                           float lambda, int max_iter);
+
+void rof_primal_dual_shared(iu::ImageGpu_32f_C1* device_f, iu::ImageGpu_32f_C1* device_u,
+                            iu::ImageGpu_32f_C1* device_u_, iu::ImageGpu_32f_C2* device_p,
+                            float lambda, int max_iter);
 
 
 // Calls
 void calcROF(iu::ImageGpu_32f_C1* f, iu::ImageGpu_32f_C1* u,
-             float lambda, int max_iter, double* init, double* alg, double* complete)
+             float lambda, int max_iter, double* init, double* alg, double* complete,
+             bool notex=false)
 {
   cudaThreadSynchronize();
   double start = iu::getTime();
@@ -45,7 +54,10 @@ void calcROF(iu::ImageGpu_32f_C1* f, iu::ImageGpu_32f_C1* u,
   double interm = iu::getTime();
   *init = interm - start;
 
-  rof_primal_dual(f, u, &u_, &p, lambda, max_iter);
+  if (notex)
+    rof_primal_dual_notex(f, u, &u_, &p, lambda, max_iter);
+  else
+    rof_primal_dual(f, u, &u_, &p, lambda, max_iter);
 
   cudaThreadSynchronize();
   *alg = iu::getTime() - interm;
@@ -53,6 +65,31 @@ void calcROF(iu::ImageGpu_32f_C1* f, iu::ImageGpu_32f_C1* u,
 
   return;
 }
+
+void calcROFshared(iu::ImageGpu_32f_C1* f, iu::ImageGpu_32f_C1* u,
+                   float lambda, int max_iter, double* init, double* alg, double* complete)
+{
+  cudaThreadSynchronize();
+  double start = iu::getTime();
+
+  iu::ImageGpu_32f_C1 u_(u->size());
+  iu::copy(u, &u_);
+  iu::ImageGpu_32f_C2 p(u->size());
+  iu::setValue(make_float2(0.0f, 0.0f), &p, p.roi());
+
+  cudaThreadSynchronize();
+  double interm = iu::getTime();
+  *init = interm - start;
+
+  rof_primal_dual_shared(f, u, &u_, &p, lambda, max_iter);
+
+  cudaThreadSynchronize();
+  *alg = iu::getTime() - interm;
+  *complete = iu::getTime() - start;
+
+  return;
+}
+
 
 void calcROFSparse(iu::ImageGpu_32f_C1* f, iu::ImageGpu_32f_C1* u,
                    float lambda, int max_iter, double* init, double* alg, double* complete)
@@ -68,9 +105,6 @@ void calcROFSparse(iu::ImageGpu_32f_C1* f, iu::ImageGpu_32f_C1* u,
   iu::copy(u, &u_);
   iu::ImageGpu_32f_C2 p(u->size());
   iu::setValue(make_float2(0.0f, 0.0f), &p, p.roi());
-
-//  std::cout << "u: " << u->width() << "x" << u->height() << "  Stride=" << u->stride() << std::endl;
-//  std::cout << "p: " << p.width() << "x" << p.height() << "  Stride=" << p.stride() << std::endl;
 
   // Generate Matrix G
   iu::LinearHostMemory<int> rowG(u->stride()*u->height()*4);
@@ -116,6 +150,8 @@ void calcROFSparse(iu::ImageGpu_32f_C1* f, iu::ImageGpu_32f_C1* u,
   iu::copy(&valG, &valG_d);
   iu::SparseMatrixGpu_32f G_d(&handle, &valG_d, &rowG_d, p.stride()*2*p.height(), &colG_d, u->stride()*u->height());
 
+  G_d.changeSparseFormat(CSR);
+
   cudaThreadSynchronize();
   double interm = iu::getTime();
   *init = interm - start;
@@ -126,19 +162,17 @@ void calcROFSparse(iu::ImageGpu_32f_C1* f, iu::ImageGpu_32f_C1* u,
   *alg = iu::getTime() - interm;
   *complete = iu::getTime() - start;
 
-    cusparseDestroy(handle);
+  cusparseDestroy(handle);
 
   return;
 }
 
 
-
-
 //////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[])
 {
-//  // Sparse matrix
-//  cusparseCreate(&handle);
+  //  // Sparse matrix
+  //  cusparseCreate(&handle);
 
   // full
   iu::ImageGpu_32f_C1* input_full = iu::imread_cu32f_C1("../../Data/test/cat.pgm");
@@ -147,16 +181,24 @@ int main(int argc, char *argv[])
   iu::reduce(input_full, &input_256);
 
   iu::ImageGpu_32f_C1 output_256(input_256.size());
+  iu::ImageGpu_32f_C1 output_256_notex(input_256.size());
+  iu::ImageGpu_32f_C1 output_256_shared(input_256.size());
   iu::ImageGpu_32f_C1 output_256_sparse(input_256.size());
   iu::copy(&input_256, &output_256);
+  iu::copy(&input_256, &output_256_notex);
+  iu::copy(&input_256, &output_256_shared);
   iu::copy(&input_256, &output_256_sparse);
 
   iu::ImageGpu_32f_C1 input_1024(1024, 1024*input_full->height()/input_full->width());
   iu::reduce(input_full, &input_1024);
 
   iu::ImageGpu_32f_C1 output_1024(input_1024.size());
+  iu::ImageGpu_32f_C1 output_1024_notex(input_1024.size());
+  iu::ImageGpu_32f_C1 output_1024_shared(input_1024.size());
   iu::ImageGpu_32f_C1 output_1024_sparse(input_1024.size());
   iu::copy(&input_1024, &output_1024);
+  iu::copy(&input_1024, &output_1024_notex);
+  iu::copy(&input_1024, &output_1024_shared);
   iu::copy(&input_1024, &output_1024_sparse);
 
   float lambda    = 1.0f;
@@ -165,16 +207,37 @@ int main(int argc, char *argv[])
   double init[NSIZES][NALG];
   double alg[NSIZES][NALG];
   double complete[NSIZES][NALG];
+  for (int a=0; a<NALG; a++)
+  {
+    for(int sz=0; sz<NSIZES; sz++)
+    {
+      init[sz][a] = -1.0;
+      alg[sz][a] = -1.0;
+      complete[sz][a] = -1.0;
+    }
+  }
+  char* name[] = {"Standard: ", "No Tex:   ", "Shared:  ", "Sparse:   "};
 
-  char* name[] = {"Standard: ", "Sparse:   "};
-
+  int calg = 0;
   // Co calculations of standard ROF model
-  calcROF(&input_256, &output_256,  lambda, max_iter, &init[0][0], &alg[0][0], &complete[0][0]);
-  calcROF(&input_1024, &output_1024, lambda, max_iter, &init[1][0], &alg[1][0], &complete[1][0]);
+  calcROF(&input_256, &output_256,  lambda, max_iter, &init[0][calg], &alg[0][calg], &complete[0][calg]);
+  calcROF(&input_1024, &output_1024, lambda, max_iter, &init[1][calg], &alg[1][calg], &complete[1][calg]);
+  calg++;
 
-  // Co calculations of sparse ROF model
-  calcROFSparse(&input_256, &output_256_sparse,  lambda, max_iter, &init[0][1], &alg[0][1], &complete[0][1]);
-  calcROFSparse(&input_1024, &output_1024_sparse, lambda, max_iter, &init[1][1], &alg[1][1], &complete[1][1]);
+  // Co calculations of standard ROF with no texture model
+  calcROF(&input_256, &output_256_notex,  lambda, max_iter, &init[0][calg], &alg[0][calg], &complete[0][calg], true);
+  calcROF(&input_1024, &output_1024_notex, lambda, max_iter, &init[1][calg], &alg[1][calg], &complete[1][calg], true);
+  calg++;
+
+  // Co calculations of standard ROF with shared memory
+  calcROFshared(&input_256, &output_256_shared,  lambda, max_iter, &init[0][calg], &alg[0][calg], &complete[0][calg]);
+  calcROFshared(&input_1024, &output_1024_shared, lambda, max_iter, &init[1][calg], &alg[1][calg], &complete[1][calg]);
+  calg++;
+
+  //  // Co calculations of sparse ROF model
+  //  calcROFSparse(&input_256, &output_256_sparse,  lambda, max_iter, &init[0][calg], &alg[0][calg], &complete[0][calg]);
+  //  calcROFSparse(&input_1024, &output_1024_sparse, lambda, max_iter, &init[1][calg], &alg[1][calg], &complete[1][calg]);
+  //  calg++;
 
   std::cout << "          256              1024" << std::endl;
   for (int a=0; a<NALG; a++)
@@ -190,15 +253,19 @@ int main(int argc, char *argv[])
 
   iu::imsave(&input_256, "256_input.png");
   iu::imsave(&output_256, "256_output.png");
+  iu::imsave(&output_256_notex, "256_notex_output.png");
+  iu::imsave(&output_256_shared, "256_shared_output.png");
   iu::imsave(&output_256_sparse, "256_sparse_output.png");
 
   iu::imsave(&input_1024, "1024_input.png");
   iu::imsave(&output_1024, "1024_output.png");
+  iu::imsave(&output_1024_notex, "1024_notex_output.png");
+  iu::imsave(&output_1024_shared, "1024_shared_output.png");
   iu::imsave(&output_1024_sparse, "1024_sparse_output.png");
 
 
   // Clean up
-//  cusparseDestroy(handle);
+  //  cusparseDestroy(handle);
   delete input_full;
 
   return 0;
--- tests/iusparse_unittests/iu_sparse_compare.cu
+++ tests/iusparse_unittests/iu_sparse_compare.cu
@@ -17,7 +17,8 @@ texture<float2, 2, cudaReadModeElementType> rof_tex_p;
 texture<float2, 2, cudaReadModeElementType> rof_tex_gradient;
 texture<float, 2, cudaReadModeElementType> rof_tex_divergence;
 
-const unsigned int ROF_BLOCK_SIZE=16;
+const unsigned int BSX=16;
+const unsigned int BSY=16;
 
 ////////////////////////////////////////////////////////////////////////////////
 void bindTexture(texture<float, 2>& tex, iu::ImageGpu_32f_C1* mem)
@@ -28,7 +29,7 @@ void bindTexture(texture<float, 2>& tex, iu::ImageGpu_32f_C1* mem)
   tex.normalized = false;
   cudaChannelFormatDesc channel_desc = cudaCreateChannelDesc<float>();
   ( cudaBindTexture2D( 0, &tex, mem->data(), &channel_desc,
-                       mem->width(), mem->height(), mem->pitch()));
+                      mem->width(), mem->height(), mem->pitch()));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -40,7 +41,7 @@ void bindTexture(texture<float2, 2>& tex, iu::ImageGpu_32f_C2* mem)
   tex.normalized = false;
   cudaChannelFormatDesc channel_desc_float2 = cudaCreateChannelDesc<float2>();
   ( cudaBindTexture2D( 0, &tex, mem->data(), &channel_desc_float2,
-                       mem->width(), mem->height(), mem->pitch()));
+                      mem->width(), mem->height(), mem->pitch()));
 }
 
 
@@ -93,19 +94,19 @@ __global__ void dual_energy_kernel(float* dual, float lambda,
 
 ////////////////////////////////////////////////////////////////////////////////
 void rof_energy( iu::ImageGpu_32f_C1* device_f, iu::ImageGpu_32f_C1* device_u,
-                 iu::ImageGpu_32f_C2* device_p,
-                 float lambda, double& primal_energy, double& dual_energy)
+                iu::ImageGpu_32f_C2* device_p,
+                float lambda, double& primal_energy, double& dual_energy)
 {
   int width = device_f->width();
   int height = device_f->height();
 
   // compute number of Blocks
-  int nb_x = width/ROF_BLOCK_SIZE;
-  int nb_y = height/ROF_BLOCK_SIZE;
-  if (nb_x*ROF_BLOCK_SIZE < width) nb_x++;
-  if (nb_y*ROF_BLOCK_SIZE < height) nb_y++;
+  int nb_x = width/BSX;
+  int nb_y = height/BSY;
+  if (nb_x*BSX < width) nb_x++;
+  if (nb_y*BSY < height) nb_y++;
 
-  dim3 dimBlock(ROF_BLOCK_SIZE,ROF_BLOCK_SIZE);
+  dim3 dimBlock(BSX,BSY);
   dim3 dimGrid(nb_x,nb_y);
 
   bindTexture(rof_tex_f, device_f);
@@ -117,18 +118,23 @@ void rof_energy( iu::ImageGpu_32f_C1* device_f, iu::ImageGpu_32f_C1* device_u,
 
   // Calculate primal energy
   primal_energy_kernel
-      <<< dimGrid, dimBlock >>> (temp_energy.data(), lambda,
-                                 width, height, temp_energy.stride());
+      <<<dimGrid, dimBlock>>>(temp_energy.data(), lambda,
+                              width, height, temp_energy.stride());
   iu::summation(&temp_energy, device_f->roi(), primal_energy);
 
   // Calculate dual energy
   dual_energy_kernel
-      <<< dimGrid, dimBlock >>> (temp_energy.data(), lambda,
-                                 width, height, temp_energy.stride());
+      <<<dimGrid, dimBlock>>>(temp_energy.data(), lambda,
+                              width, height, temp_energy.stride());
   iu::summation(&temp_energy, device_f->roi(), dual_energy);
   iu::checkCudaErrorState();
 }
 
+////////////////////////////////////////////////////////////////////////////////
+//############################################################################//
+//OOOOOOOOOOOOOOOOOOOOOOOOOOO  STANDARD MODEL  OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO//
+//############################################################################//
+////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
 __global__ void update_primal(float* device_u, float* device_u_,
@@ -187,10 +193,13 @@ void rof_primal_dual(iu::ImageGpu_32f_C1* device_f, iu::ImageGpu_32f_C1* device_
   int height = device_f->height();
 
   // fragmentation
-  unsigned int block_size = ROF_BLOCK_SIZE;
-  dim3 dimBlock(block_size, block_size);
-  dim3 dimGrid(iu::divUp(width, dimBlock.x),
-               iu::divUp(height, dimBlock.y));
+  int nb_x = width/BSX;
+  int nb_y = height/BSY;
+  if (nb_x*BSX < width) nb_x++;
+  if (nb_y*BSY < height) nb_y++;
+
+  dim3 dimBlock(BSX,BSY);
+  dim3 dimGrid(nb_x,nb_y);
 
   bindTexture(rof_tex_f, device_f);
   bindTexture(rof_tex_u, device_u);
@@ -204,17 +213,17 @@ void rof_primal_dual(iu::ImageGpu_32f_C1* device_f, iu::ImageGpu_32f_C1* device_
 
   for (int k = 0; k < max_iter; ++k)
   {
-    update_dual <<< dimGrid, dimBlock >>> (device_p->data(), sigma,
-                                           width, height, device_p->stride());
+    update_dual<<<dimGrid, dimBlock>>>(device_p->data(), sigma,
+                                       width, height, device_p->stride());
 
     if (sigma < 1000.0f)
       theta = 1/sqrt(1.0f+0.7f*lambda*tau);
     else
       theta = 1.0f;
 
-    update_primal <<< dimGrid, dimBlock >>> (device_u->data(), device_u_->data(),
-                                             tau, theta, lambda, width, height,
-                                             device_u->stride());
+    update_primal<<<dimGrid, dimBlock>>>(device_u->data(), device_u_->data(),
+                                         tau, theta, lambda, width, height,
+                                         device_u->stride());
     sigma /= theta;
     tau *= theta;
   }
@@ -225,6 +234,127 @@ void rof_primal_dual(iu::ImageGpu_32f_C1* device_f, iu::ImageGpu_32f_C1* device_
   }
 }
 
+////////////////////////////////////////////////////////////////////////////////
+//############################################################################//
+//OOOOOOOOOOOOOOOOOOOOOOOOOOOOO  NO TEXTURE  OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO//
+//############################################################################//
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+__global__ void update_primal_notex(float* device_u, float* device_u_,
+                                    float* device_f, float2* device_p,
+                                    float tau, float theta, float lambda,
+                                    int width, int height, int xstride, int xstride2)
+{
+  int x = blockIdx.x*blockDim.x + threadIdx.x;
+  int y = blockIdx.y*blockDim.y + threadIdx.y;
+  int c = y*xstride + x;
+
+  if(x<width && y<height)
+  {
+    // texture fetches
+    float f = device_f[c];
+    float u = device_u[c];
+    float u_ = u;
+
+    float2 cval = device_p[y*xstride2 + x];
+    float2 wval = device_p[y*xstride2 + max(0,x-1)];
+    float2 nval = device_p[max(0,y-1)*xstride2 + x];
+    if (x == 0)
+      wval.x = 0.0f;
+    else if (x >= width-1)
+      cval.x = 0.0f;
+    if (y == 0)
+      nval.y = 0.0f;
+    else if (y >= height-1)
+      cval.y = 0.0f;
+    float divergence = cval.x - wval.x + cval.y - nval.y;
+
+    // update primal variable
+    u = (u + tau*(divergence + lambda*f))/(1.0f+tau*lambda);
+
+    device_u[c] = u;
+    device_u_[c] = u + theta*(u-u_);
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+__global__ void update_dual_notex(float2* device_p, float* device_u_, float sigma,
+                                  int width, int height, int xstride, int xstride2)
+{
+  int x = blockIdx.x*blockDim.x + threadIdx.x;
+  int y = blockIdx.y*blockDim.y + threadIdx.y;
+  int c2 = y*xstride2 + x;
+
+  if(x<width && y<height)
+  {
+    float2 p = device_p[c2];
+    float2 grad_u = make_float2(0.0f, 0.0f);
+    float cval = device_u_[y*xstride + x];
+    grad_u.x = device_u_[y*xstride + min(width-1,x+1)] - cval;
+    grad_u.y = device_u_[min(height-1,y+1)*xstride + x] - cval;
+
+    // update dual variable
+    p = p + sigma*grad_u;
+    p = p/max(1.0f, length(p));
+
+    device_p[c2] = p;
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+void rof_primal_dual_notex(iu::ImageGpu_32f_C1* device_f, iu::ImageGpu_32f_C1* device_u,
+                           iu::ImageGpu_32f_C1* device_u_, iu::ImageGpu_32f_C2* device_p,
+                           float lambda, int max_iter)
+{
+  int width = device_f->width();
+  int height = device_f->height();
+
+  // fragmentation
+  int nb_x = width/BSX;
+  int nb_y = height/BSY;
+  if (nb_x*BSX < width) nb_x++;
+  if (nb_y*BSY < height) nb_y++;
+
+  dim3 dimBlock(BSX,BSY);
+  dim3 dimGrid(nb_x,nb_y);
+
+  float L = sqrt(8.0f);
+  float tau = 1/L;
+  float sigma = 1/L;
+  float theta;
+
+  for (int k = 0; k < max_iter; ++k)
+  {
+    update_dual_notex<<<dimGrid, dimBlock>>>(device_p->data(), device_u_->data(),
+                                             sigma, width, height,
+                                             device_u_->stride(), device_p->stride());
+
+    if (sigma < 1000.0f)
+      theta = 1/sqrt(1.0f+0.7f*lambda*tau);
+    else
+      theta = 1.0f;
+
+    update_primal_notex<<<dimGrid, dimBlock>>>(device_u->data(), device_u_->data(),
+                                               device_f->data(), device_p->data(),
+                                               tau, theta, lambda, width, height,
+                                               device_u->stride(), device_p->stride());
+    sigma /= theta;
+    tau *= theta;
+  }
+  IuStatus status = iu::checkCudaErrorState(true);
+  if(status != IU_NO_ERROR)
+  {
+    std::cerr << "An error occured while solving the ROF model." << std::endl;
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//############################################################################//
+//OOOOOOOOOOOOOOOOOOOOOOOOOOO  SPARSE MATRIX  OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO//
+//############################################################################//
+////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
 __global__ void update_primal_sparse(float* device_u, float* device_u_,
@@ -274,10 +404,13 @@ void rof_primal_dual_sparse(iu::SparseMatrixGpu_32f* G,
   int height = device_f->height();
 
   // fragmentation
-  unsigned int block_size = ROF_BLOCK_SIZE;
-  dim3 dimBlock(block_size, block_size);
-  dim3 dimGrid(iu::divUp(width, dimBlock.x),
-               iu::divUp(height, dimBlock.y));
+  int nb_x = width/BSX;
+  int nb_y = height/BSY;
+  if (nb_x*BSX < width) nb_x++;
+  if (nb_y*BSY < height) nb_y++;
+
+  dim3 dimBlock(BSX,BSY);
+  dim3 dimGrid(nb_x,nb_y);
 
   iu::ImageGpu_32f_C2 gradient(width, height);
   iu::ImageGpu_32f_C1 divergence(width, height);
@@ -322,6 +455,146 @@ void rof_primal_dual_sparse(iu::SparseMatrixGpu_32f* G,
     cudaUnbindTexture(&rof_tex_divergence);
 
     // Update Timesteps
+    sigma /= theta;
+    tau *= theta;
+  }
+  IuStatus status = iu::checkCudaErrorState(true);
+  if(status != IU_NO_ERROR)
+  {
+    std::cerr << "An error occured while solving the ROF model." << std::endl;
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//############################################################################//
+//OOOOOOOOOOOOOOOOOOOOOOOOOOO  SHARED MEMORY  OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO//
+//############################################################################//
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+__global__ void update_primal_shared(float* device_u, float* device_u_,
+                                     float tau, float theta, float lambda,
+                                     int width, int height, int xstride)
+{
+  int x = blockIdx.x*blockDim.x + threadIdx.x;
+  int y = blockIdx.y*blockDim.y + threadIdx.y;
+  int c = y*xstride + x;
+
+  if(x<width && y<height)
+  {
+    // texture fetches
+    float f = tex2D(rof_tex_f, x+0.5f, y+0.5f);
+    float u = tex2D(rof_tex_u, x+0.5f, y+0.5f);
+    float u_ = u;
+
+    __shared__ float2 p[BSX+1][BSY+1];
+
+    p[threadIdx.x+1][threadIdx.y+1] = tex2D(rof_tex_p, x+0.5f, y+0.5f);
+
+    if (threadIdx.x==0)
+    {
+      p[threadIdx.x][threadIdx.y+1] = tex2D(rof_tex_p, x-0.5f, y+0.5f);
+      if (x==0)
+        p[threadIdx.x][threadIdx.y+1].x = 0.0f;
+    }
+    if (threadIdx.y==0)
+    {
+      p[threadIdx.x+1][threadIdx.y] = tex2D(rof_tex_p, x+0.5f, y-0.5f);
+      if (y==0)
+        p[threadIdx.x+1][threadIdx.y].y = 0.0f;
+    }
+
+    if (x >= width-1)
+      p[threadIdx.x+1][threadIdx.y+1].x = 0.0f;
+    if (y >= height-1)
+      p[threadIdx.x+1][threadIdx.y+1].y = 0.0f;
+
+//        syncthreads();
+
+    float divergence = p[threadIdx.x+1][threadIdx.y+1].x - p[threadIdx.x][threadIdx.y+1].x +
+                       p[threadIdx.x+1][threadIdx.y+1].y - p[threadIdx.x+1][threadIdx.y].y;
+
+    // update primal variable
+    u = (u + tau*(divergence + lambda*f))/(1.0f+tau*lambda);
+
+    device_u[c] = u;
+    device_u_[c] = u + theta*(u-u_);
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+__global__ void update_dual_shared(float2* device_p, float sigma,
+                                   int width, int height, int stride)
+{
+  int x = blockIdx.x*blockDim.x + threadIdx.x;
+  int y = blockIdx.y*blockDim.y + threadIdx.y;
+
+  if(x<width && y<height)
+  {
+    float2 p = tex2D(rof_tex_p, x+0.5f, y+0.5f);
+
+    __shared__ float u_[BSX+1][BSY+1];
+    u_[threadIdx.x][threadIdx.y] = tex2D(rof_tex_u_, x+0.5f, y+0.5f);
+
+    if (threadIdx.x==BSX-1)
+      u_[threadIdx.x+1][threadIdx.y] = tex2D(rof_tex_u_, x+1.5f, y+0.5f);
+    if (threadIdx.y==BSY-1)
+      u_[threadIdx.x][threadIdx.y+1] = tex2D(rof_tex_u_, x+0.5f, y+1.5f);
+
+//    syncthreads();
+
+    float2 grad_u = make_float2(u_[threadIdx.x+1][threadIdx.y] - u_[threadIdx.x][threadIdx.y],
+                                u_[threadIdx.x][threadIdx.y+1] - u_[threadIdx.x][threadIdx.y] );
+
+    // update dual variable
+    p = p + sigma*grad_u;
+    p = p/max(1.0f, length(p));
+
+    device_p[y*stride + x] = p;
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+void rof_primal_dual_shared(iu::ImageGpu_32f_C1* device_f, iu::ImageGpu_32f_C1* device_u,
+                            iu::ImageGpu_32f_C1* device_u_, iu::ImageGpu_32f_C2* device_p,
+                            float lambda, int max_iter)
+{
+  int width = device_f->width();
+  int height = device_f->height();
+
+  // fragmentation
+  int nb_x = width/BSX;
+  int nb_y = height/BSY;
+  if (nb_x*BSX < width) nb_x++;
+  if (nb_y*BSY < height) nb_y++;
+
+  dim3 dimBlock(BSX,BSY);
+  dim3 dimGrid(nb_x,nb_y);
+
+  bindTexture(rof_tex_f, device_f);
+  bindTexture(rof_tex_u, device_u);
+  bindTexture(rof_tex_u_, device_u_);
+  bindTexture(rof_tex_p, device_p);
+
+  float L = sqrt(8.0f);
+  float tau = 1/L;
+  float sigma = 1/L;
+  float theta;
+
+  for (int k = 0; k < max_iter; ++k)
+  {
+    update_dual_shared<<<dimGrid, dimBlock>>>(device_p->data(), sigma,
+                                              width, height, device_p->stride());
+
+    if (sigma < 1000.0f)
+      theta = 1/sqrt(1.0f+0.7f*lambda*tau);
+    else
+      theta = 1.0f;
+
+    update_primal_shared<<<dimGrid, dimBlock>>>(device_u->data(), device_u_->data(),
+                                                tau, theta, lambda, width, height,
+                                                device_u->stride());
     sigma /= theta;
     tau *= theta;
   }
