d25eec0a090b025dba58b9403c8e5239d5b8f364
werlberger <werlberger@358b74ad-2626-0410-af80-f4c3c349ae55>
2011-09-07 07:23:55 +0000

    added general header file for container type data structures (e.g. image vectors, image pyramids, ...)
    git-svn-id: https://svn.icg.tugraz.at/svn/vmgpu/trunk/vmlibraries/imageutilities@11535 358b74ad-2626-0410-af80-f4c3c349ae55

---
+++ src/iucontainers.h
@@ -0,0 +1,20 @@
+#ifndef IU_CONTAINERS_H
+#define IU_CONTAINERS_H
+
+#include <vector>
+#include <deque>
+
+#include "iucore/memorydefs.h"
+#include "iucore/imagepyramid.h"
+
+
+namespace iu {
+
+//  Image and Pyramid 'lists' implemented as double-ended queue
+typedef std::vector<iu::Image*> ImageVector;
+typedef std::deque<iu::Image*> ImageDeque;
+typedef std::deque<iu::ImagePyramid*> ImagePyramidDeque;
+
+} // namespace iu
+
+#endif // IU_CONTAINERS_H
