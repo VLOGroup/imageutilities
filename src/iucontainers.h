2270a7a899254a975afb4e010aeb6e858642cfca
werlberger <werlberger@358b74ad-2626-0410-af80-f4c3c349ae55>
2011-10-22 14:16:25 +0000

    added library for computing trajectory flow. finished up to kernel calls. (hopefully)
    git-svn-id: https://svn.icg.tugraz.at/svn/vmgpu/trunk/vmlibraries/imageutilities@11759 358b74ad-2626-0410-af80-f4c3c349ae55

--- src/iucontainers.h
+++ src/iucontainers.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <deque>
+#include <map>
 
 #include "iucore/memorydefs.h"
 #include "iucore/imagepyramid.h"
@@ -13,7 +14,12 @@ namespace iu {
 // Image and Pyramid 'lists' implemented as double-ended queue
 typedef std::vector<iu::Image*> ImageVector;
 typedef std::deque<iu::Image*> ImageDeque;
+typedef std::deque<iu::ImageGpu_32f_C1*> ImageGpuDeque_32f_C1;
+typedef std::deque<iu::ImageGpu_32f_C2*> ImageGpuDeque_32f_C2;
 typedef std::deque<iu::ImagePyramid*> ImagePyramidDeque;
+typedef std::map<float,iu::Image*> TimeImageMap;
+typedef std::deque<iu::ImagePyramid*> ImagePyramidDeque;
+typedef std::map<float,iu::ImagePyramid*> TimeImagePyramidMap;
 
 } // namespace iu
 
