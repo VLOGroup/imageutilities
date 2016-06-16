be9043dbcb46c11a880e928d50334ec95da15263
Christian Reinbacher <reinbacher@icg.tugraz.at>
2016-06-16 08:10:51 +0200

    added arithmetics for linear memory

--- src/iumath/arithmetics.cuh
+++ src/iumath/arithmetics.cuh
@@ -23,6 +23,13 @@ void addC(iu::ImageGpu<PixelType, iuprivate::ImageAllocatorGpu<Allocator> >& src
     thrust::transform(src.begin(),src.end(),thrust::constant_iterator<PixelType>(val),dst.begin(),thrust::plus<PixelType>());
 }
 
+template<typename PixelType>
+void addC(iu::LinearDeviceMemory<PixelType>& src, const PixelType& val,
+          iu::LinearDeviceMemory<PixelType>& dst)
+{
+    thrust::transform(src.begin(),src.end(),thrust::constant_iterator<PixelType>(val),dst.begin(),thrust::plus<PixelType>());
+}
+
 /** Multiplication to every pixel of a constant value. (can be called in-place)
  * \param src Source image.
  * \param val Value to be added.
@@ -31,6 +38,13 @@ void addC(iu::ImageGpu<PixelType, iuprivate::ImageAllocatorGpu<Allocator> >& src
 template<typename PixelType, class Allocator >
 void mulC(iu::ImageGpu<PixelType, iuprivate::ImageAllocatorGpu<Allocator> >& src, const PixelType& val,
                         iu::ImageGpu<PixelType, iuprivate::ImageAllocatorGpu<Allocator> >& dst)
+{
+    thrust::transform(src.begin(),src.end(),thrust::constant_iterator<PixelType>(val),dst.begin(),thrust::multiplies<PixelType>());
+}
+
+template<typename PixelType>
+void mulC(iu::LinearDeviceMemory<PixelType>& src, const PixelType& val,
+                        iu::LinearDeviceMemory<PixelType>& dst)
 {
     thrust::transform(src.begin(),src.end(),thrust::constant_iterator<PixelType>(val),dst.begin(),thrust::multiplies<PixelType>());
 }
@@ -51,6 +65,39 @@ void addWeighted(iu::ImageGpu<PixelType, iuprivate::ImageAllocatorGpu<Allocator>
                       thrust::make_zip_iterator(thrust::make_tuple(src1.begin(), src2.begin())) + src1.stride()*src1.height(),
                       dst.begin(),
                       unary_op);
+}
+
+template<typename PixelType>
+void addWeighted(iu::LinearDeviceMemory<PixelType>& src1, const PixelType& weight1,
+                 iu::LinearDeviceMemory<PixelType>& src2, const PixelType& weight2,
+                 iu::LinearDeviceMemory<PixelType>& dst)
+{
+    weightedsum_transform_tuple<PixelType> unary_op(weight1,weight2); // transformation operator
+    thrust::transform(thrust::make_zip_iterator(thrust::make_tuple(src1.begin(), src2.begin())),
+                      thrust::make_zip_iterator(thrust::make_tuple(src1.begin(), src2.begin())) + src1.length(),
+                      dst.begin(),
+                      unary_op);
+}
+
+/** Multiplying an image to another.
+ * \param src1 Source image 1.
+ * \param src2 Source image 2.
+ * \param dst Result image dst=src1*src2.
+ */
+template<typename PixelType, class Allocator >
+void mul(iu::ImageGpu<PixelType, iuprivate::ImageAllocatorGpu<Allocator> >& src1,
+         iu::ImageGpu<PixelType, iuprivate::ImageAllocatorGpu<Allocator> >& src2,
+         iu::ImageGpu<PixelType, iuprivate::ImageAllocatorGpu<Allocator> >& dst)
+{
+    thrust::transform(src1.begin(), src1.end(), src2.begin(), dst.begin(), thrust::multiplies<PixelType>());
+}
+
+template<typename PixelType>
+void mul(iu::LinearDeviceMemory<PixelType>& src1,
+         iu::LinearDeviceMemory<PixelType>& src2,
+         iu::LinearDeviceMemory<PixelType>& dst)
+{
+    thrust::transform(src1.begin(), src1.end(), src2.begin(), dst.begin(), thrust::multiplies<PixelType>());
 }
 
 }//namespace math
--- tests/mathTest/main.cpp
+++ tests/mathTest/main.cpp
@@ -12,8 +12,11 @@ int main()
     //	iu::copy(&I1,&I2);
     iu::ImageGpu_32f_C1 *I1 = iu::imread_cu32f_C1(DATA_PATH("army_1.png"));
     iu::ImageGpu_32f_C1 *I2 = iu::imread_cu32f_C1(DATA_PATH("army_2.png"));
+    iu::ImageGpu_32f_C1 result(I1->size());
 
-    iu::math::addC(*I1,0.5,*I2);
-    iu::imsave(I2,RESULTS_PATH("army_2.png"));
+    iu::math::addC(*I1,0.5,result);
+    iu::imsave(&result,RESULTS_PATH("army_1_bright.png"));
+    iu::math::addWeighted(*I1,1,*I2,-1,result);
+    iu::imsave(&result,RESULTS_PATH("army_1_minus_2.png"));
     std::cout << "DONE :)" << std::endl;
 }
