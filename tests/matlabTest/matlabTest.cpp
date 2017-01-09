53aa9c2807107b5b6f8f2b0efcee4834d5068782
Kerstin Hammernik <hammernik@icg.tugraz.at>
2017-01-09 09:03:32 +0100

    Include compiler settings in ImageUtilitiesConfig. Include \bigobj flag for Windows build. Fix and test matlab/python support with Windows.

--- tests/matlabTest/matlabTest.cpp
+++ tests/matlabTest/matlabTest.cpp
@@ -3,35 +3,31 @@
 // system includes
 #include <iostream>
 
-#include "../config.h"
+#include "../../src/iumath/typetraits.h"
+#include "../../src/iumatlab.h"
+#include "../../src/iudefs.h"
 
-#include "../src/iucore.h"
-#include "../src/iumath/typetraits.h"
-#include "../src/iumatlab.h"
-#include "../src/iumath.h"
+#include "../config.h"
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-  typedef double pixel_type;
-  typedef iu::LinearHostMemory<pixel_type, 2> HostMemType;
+	// This function does not check a single thing, it just assumes two images
+	// as parameters, does something with them on the GPU and returns the result
+	char err_msg[128];
 
-  // This function does not check a single thing, it just assumes two images
-  // as parameters, does something with them on the GPU and returns the result
-  char err_msg[128];
+	// Checking number of arguments
+	if (nrhs != 1)
+		mexErrMsgIdAndTxt(
+		"MATLAB:tgvreconstruction:invalidNumInputs",
+		"One input required (2D array)");
+	if (nlhs > 1)
+		mexErrMsgIdAndTxt("MATLAB:tvreconstruction:maxlhs",
+		"Too many output arguments.");
 
-  // Checking number of arguments
-  if (nrhs != 1)
-    mexErrMsgIdAndTxt(
-        "MATLAB:tgvreconstruction:invalidNumInputs",
-        "One input required (input)");
-  if (nlhs > 1)
-    mexErrMsgIdAndTxt("MATLAB:tvreconstruction:maxlhs",
-                      "Too many output arguments.");
+	iu::LinearHostMemory<float, 2> test(*(prhs[0]));
 
-  HostMemType test(*(prhs[0]));
+	// Convert to MATLAB Output
+	iu::matlab::convertCToMatlab(test, &plhs[0]);
 
-  // Convert to MATLAB Output
-  iu::matlab::convertCToMatlab(test, &plhs[0]);
-
-  std::cout << "finished" << std::endl;
+	std::cout << "finished" << std::endl;
 }
